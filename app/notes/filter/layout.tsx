1ec1a53b80a9cbcab4a42f232e826876cabea8f8
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-04-26 12:52:14 +0300

    add render

--- app/notes/filter/layout.tsx
+++ app/notes/filter/layout.tsx
@@ -1,15 +1,7 @@
-export default function NotesFilterLayout({
+export default function NotesLayout({
   children,
-  sidebar,
 }: {
   children: React.ReactNode;
-  sidebar: React.ReactNode;
 }) {
-  return (
-    <div>
-      <aside>{sidebar}</aside>
-
-      <main>{children}</main>
-    </div>
-  );
+  return <>{children}</>;
 }
