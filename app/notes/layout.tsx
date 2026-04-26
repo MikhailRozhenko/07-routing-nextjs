bf3fe2f4c8ea63f9d2e312f32ba84c392ba7431b
Mykhailo Rozhenko <mihailkfmn@gmail.com>
2026-04-26 12:56:15 +0300

    fix modal

--- app/notes/layout.tsx
+++ app/notes/layout.tsx
@@ -1,14 +1,8 @@
 export default function NotesLayout({
   children,
-  modal,
 }: {
   children: React.ReactNode;
   modal: React.ReactNode;
 }) {
-  return (
-    <>
-      {children}
-      {modal}
-    </>
-  );
+  return <>{children}</>;
 }
